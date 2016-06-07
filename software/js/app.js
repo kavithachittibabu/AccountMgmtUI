69b6197ff5565ef13f8d9d9df151ec69c3d55315
ushadevir <usha.devi@tcs.com>
2016-06-07 20:55:48 +0530

    Update app.js

--- software/js/app.js
+++ software/js/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const ACCOUNT_CMD_HOST = 'http://54.191.4.253:32774/';
-const ACCOUNT_QUERY_HOST = 'http://54.191.4.253:32774/';
-const ACCOUNT_TRANSFER_HOST = 'http://54.191.4.253:8081/';
+const ACCOUNT_CMD_HOST = 'http://54.187.180.82:32774/';
+const ACCOUNT_QUERY_HOST = 'http://54.187.180.82:32774/';
+const ACCOUNT_TRANSFER_HOST = 'http://54.187.180.82:8081/';
 
 var AngularSpringApp = {};
 
